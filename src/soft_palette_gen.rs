0ed3241eb8fe261c453b97f4d076e3b3a5125b79
mswift42 <martin.haesler@gmail.com>
2019-10-03 16:20:39 +0100

    rename to test_is_valid

--- src/soft_palette_gen.rs
+++ src/soft_palette_gen.rs
@@ -4,14 +4,15 @@ fn is_valid(r: f64, g: f64, b: f64) -> bool {
 
 #[cfg(test)]
 mod test {
+    use super::*;
     #[test]
-    fn is_valid() {
-        assert_eq!(super::is_valid(0.0, 0.0, 0.0), true);
-        assert_eq!(super::is_valid(1.0, 1.0, 1.0), true);
-        assert_eq!(super::is_valid(1.1, 0.0, 0.0), false);
-        assert_eq!(super::is_valid(1.0, 1.1, 1.0), false);
-        assert_eq!(super::is_valid(1.0, 1.0, 0.0), true);
-        assert_eq!(super::is_valid(1.0, 0.0, 1.0), true);
-        assert_eq!(super::is_valid(1.0, 1.0, 1.1), false);
+    fn test_is_valid() {
+        assert_eq!(is_valid(0.0, 0.0, 0.0), true);
+        assert_eq!(is_valid(1.0, 1.0, 1.0), true);
+        assert_eq!(is_valid(1.1, 0.0, 0.0), false);
+        assert_eq!(is_valid(1.0, 1.1, 1.0), false);
+        assert_eq!(is_valid(1.0, 1.0, 0.0), true);
+        assert_eq!(is_valid(1.0, 0.0, 1.0), true);
+        assert_eq!(is_valid(1.0, 1.0, 1.1), false);
     }
 }
